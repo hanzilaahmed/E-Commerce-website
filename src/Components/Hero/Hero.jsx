542216b803988dd452733bc6da3e1ea845fa3a0e
hanzila123456789 <hanilaahmed90@gmail.com>
2025-09-15 13:35:11 +0500

    Fix CSS import case issue

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './hero.css'
+import './Hero.css'
 import hand_icon from '../../assets/hand_icon.png'
 import arrow_icon from '../../assets/arrow.png'
 import hero_img from '../../assets/hero_image.png'
